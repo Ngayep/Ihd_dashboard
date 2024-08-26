5815ee01f7d1914dd0531fcd9483510c499e8e67
Ngayep <courtesyofjess@gmail.com>
2024-08-26 13:19:29 +0100

    modified files

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Flask, render_template
 
-main = Blueprint('main', __name__)
+app = Flask(__name__)
 
-@main.route('/')
+@app.route('/')
 def index():
     return render_template('index.html')
